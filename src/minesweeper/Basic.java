694e159371757fa6d542ac3de32f218e2ba56327
mortii <mortteenn@gmail.com>
2015-12-16 17:13:35 +0100

    refactored Basic heavily

--- src/minesweeper/Basic.java
+++ src/minesweeper/Basic.java
@@ -3,35 +3,44 @@ package minesweeper;
 import java.util.ArrayList;
 
 public class Basic {
+	private static Square squareWithNumber;
+	private static int number;
+	private static int flags;
+	private static int nonClicked;
+	private static boolean clickedSquares;
 	
-	//http://www.minesweeper.info/wiki/Strategy
 	public static boolean doBasicSolving(){
 		//copy ArrayList to avoid concurrency issues
 		ArrayList<Integer> squaresWithNumbersCopy = new ArrayList<Integer>(Lists.squaresWithNumbers);
-		boolean clickedSquares = false;
+		clickedSquares = false;
 		
 		for (int square : squaresWithNumbersCopy){
-			Square squareWithNumber = Maps.squareMap.get(square);
-			
-			int number = squareWithNumber.numberOnSquare;
-			int flags = squareWithNumber.surroundingFlags;
-			int nonClicked = squareWithNumber.surroundingNonClickedSquares.size();
-			
-			if (number == flags){
-				Mouse.clickSurroundingNonClickedAndUpdateSurroundingSquares(squareWithNumber);
-				Lists.removeFromSquaresWithNumbers(square);
-				
-				clickedSquares = true;
-				Main.basic++;
-			}
-			else if (number == flags + nonClicked){
-				Mouse.flagSurroudingNonClicked(squareWithNumber);
-				Lists.removeFromSquaresWithNumbers(square);
-				
-				clickedSquares = true;
-				Main.basic++;
-			}
+			setClassVariables(square);
+			solve(square);
 		}
 		return clickedSquares;
+	}
+	
+	//http://www.minesweeper.info/wiki/Strategy
+	private static void solve(int square){
+		if (number == flags){
+			Mouse.clickSurroundingNonClickedAndUpdateSurroundingSquares(squareWithNumber);
+			Lists.removeFromSquaresWithNumbers(square);
+			clickedSquares = true;
+			Main.basic++;
+		}
+		else if (number == flags + nonClicked){
+			Mouse.flagSurroudingNonClicked(squareWithNumber);
+			Lists.removeFromSquaresWithNumbers(square);
+			clickedSquares = true;
+			Main.basic++;
+		}
+	}
+	
+	private static void setClassVariables(int square){
+		squareWithNumber = Maps.squareMap.get(square);
+		number = squareWithNumber.numberOnSquare;
+		flags = squareWithNumber.surroundingFlags;
+		nonClicked = squareWithNumber.surroundingNonClickedSquares.size();
 	}
 }
