4c18c8bde9ffe534e35bf1f5d9b0598b1feda9eb
mortii <mortteenn@gmail.com>
2015-12-18 01:31:43 +0100

    made squareOffset class variable

--- src/minesweeper/ComputerVision.java
+++ src/minesweeper/ComputerVision.java
@@ -14,50 +14,46 @@ public class ComputerVision {
 	private static Pixel sevenPixel = new Pixel(24, 7);
 	private static Pixel flagPixel = new Pixel(8, 10);
 	private static Pixel nonClickedPixel = new Pixel(1, 0);
-	private static HashMap<Integer, Pixel> squareCoordinates =
-			OnDisk.getHashMap("squareCoordinates.ser");
+	private static Pixel squareOffset;
+	private static HashMap<Integer, Pixel> squareCoordinates = OnDisk.getHashMap("squareCoordinates.ser");
 	
 	public static int getNumber(int square){
-		Pixel squareOffset = getSquareOffset(square);
+		squareOffset = squareCoordinates.get(square);;
 		int numberOnSquare = 0;
 		
-		if (squareIsFlag(squareOffset)){
+		if (squareIsFlag()){
 			numberOnSquare = 9;
 		}
-		else if (squareIsNonClicked(squareOffset)){
+		else if (squareIsNonClicked()){
 			numberOnSquare = 8;
 		}
-		else if (numberIsOne(squareOffset)){
+		else if (numberIsOne()){
 			numberOnSquare = 1;
 		}
-		else if (numberIsTwo(squareOffset)){
+		else if (numberIsTwo()){
 			numberOnSquare = 2;
 		}		
-		else if (numberIsThree(squareOffset)){
+		else if (numberIsThree()){
 			numberOnSquare = 3;
 		}
-		else if (numberIsFour(squareOffset)){
+		else if (numberIsFour()){
 			numberOnSquare = 4;
 		}
-		else if (numberIsFive(squareOffset)){
+		else if (numberIsFive()){
 			numberOnSquare = 5;
 		}
-		else if (numberIsSix(squareOffset)){
+		else if (numberIsSix()){
 			numberOnSquare = 6;
 		}
-		else if (numberIsSeven(squareOffset)){
+		else if (numberIsSeven()){
 			numberOnSquare = 7;
 		}
 		
 		return numberOnSquare;
 	}
-	
-	private static Pixel getSquareOffset(int square){
-		return squareCoordinates.get(square);
-	}
 
-	private static boolean squareIsNonClicked(Pixel squareOffset){
-		PixelColors nonClickedPixelColors = new PixelColors(nonClickedPixel, squareOffset);
+	private static boolean squareIsNonClicked(){
+		PixelColors nonClickedPixelColors = new PixelColors(nonClickedPixel);
 		
 		if (nonClickedPixelColors.blue > 40){
 			return true;
@@ -65,8 +61,8 @@ public class ComputerVision {
 		return false;
 	}
 
-	private static boolean squareIsFlag(Pixel squareOffset){
-		PixelColors flagPixelColors = new PixelColors(flagPixel, squareOffset);
+	private static boolean squareIsFlag(){
+		PixelColors flagPixelColors = new PixelColors(flagPixel);
 		
 		if (flagPixelColors.blue < 50){
 			return true;
@@ -74,8 +70,8 @@ public class ComputerVision {
 		return false;
 	}
 
-	private static boolean numberIsOne(Pixel squareOffset){
-		PixelColors onePixelColors = new PixelColors(onePixel, squareOffset);
+	private static boolean numberIsOne(){
+		PixelColors onePixelColors = new PixelColors(onePixel);
 		
 		if (onePixelColors.red < 75 && onePixelColors.red > 60 &&
 				onePixelColors.green < 100){
@@ -84,8 +80,8 @@ public class ComputerVision {
 		return false;
 	}
 	
-	private static boolean numberIsTwo(Pixel squareOffset){
-		PixelColors twoPixelColors = new PixelColors(twoPixel, squareOffset);
+	private static boolean numberIsTwo(){
+		PixelColors twoPixelColors = new PixelColors(twoPixel);
 		
 		if (twoPixelColors.blue < 10){
 			return true;
@@ -93,8 +89,8 @@ public class ComputerVision {
 		return false;
 	}
 	
-	private static boolean numberIsThree(Pixel squareOffset){
-		PixelColors threePixelColors = new PixelColors(threePixel, squareOffset);
+	private static boolean numberIsThree(){
+		PixelColors threePixelColors = new PixelColors(threePixel);
 		
 		if (threePixelColors.blue < 20 && threePixelColors.green < 20){
 			return true;
@@ -102,8 +98,8 @@ public class ComputerVision {
 		return false;
 	}
 	
-	private static boolean numberIsFour(Pixel squareOffset){
-		PixelColors fourPixelColors = new PixelColors(fourPixel, squareOffset);
+	private static boolean numberIsFour(){
+		PixelColors fourPixelColors = new PixelColors(fourPixel);
 		
 		if (fourPixelColors.red < 5){
 			return true;
@@ -111,8 +107,8 @@ public class ComputerVision {
 		return false;
 	}
 	
-	private static boolean numberIsFive(Pixel squareOffset){
-		PixelColors fivePixelColors = new PixelColors(fivePixel, squareOffset);
+	private static boolean numberIsFive(){
+		PixelColors fivePixelColors = new PixelColors(fivePixel);
 		
 		if (fivePixelColors.red < 200 && fivePixelColors.green < 10 &&
 				fivePixelColors.blue < 10){
@@ -121,8 +117,8 @@ public class ComputerVision {
 		return false;
 	}
 	
-	private static boolean numberIsSix(Pixel squareOffset){
-		PixelColors sixPixelColors = new PixelColors(sixPixel, squareOffset);
+	private static boolean numberIsSix(){
+		PixelColors sixPixelColors = new PixelColors(sixPixel);
 		
 		if (sixPixelColors.red < 10 && sixPixelColors.green > 100){
 			return true;
@@ -130,38 +126,38 @@ public class ComputerVision {
 		return false;
 	}
 	
-	private static boolean numberIsSeven(Pixel squareOffset){
-		PixelColors sevenPixelColors = new PixelColors(sevenPixel, squareOffset);
+	private static boolean numberIsSeven(){
+		PixelColors sevenPixelColors = new PixelColors(sevenPixel);
 		
 		if (sevenPixelColors.green < 30){
 			return true;
 		}
 		return false;
 	}
-}
-
-class PixelColors {
-	public int red;
-	public int green;
-	public int blue;
 	
-	public PixelColors(Pixel identityPixel, Pixel squareOffset) {
-		Pixel realPixel = getRealPixel(identityPixel, squareOffset);
-		BufferedImage boardImage = Board.getBoardImage();
-		Color pixelColors = new Color(boardImage.getRGB(realPixel.x, realPixel.y));
-		setPixelColors(pixelColors);
-	}
-	
-	private Pixel getRealPixel(Pixel identityPixel, Pixel squareOffset){
-		Pixel realPixel = new Pixel(identityPixel.x, identityPixel.y);
-		realPixel.x += squareOffset.x;
-		realPixel.y += squareOffset.y;
-		return realPixel;
-	}
-	
-	private void setPixelColors(Color pixelColors){
-		this.red = pixelColors.getRed();
-		this.green = pixelColors.getGreen();
-		this.blue = pixelColors.getBlue();
+	private static class PixelColors{
+		public int red;
+		public int green;
+		public int blue;
+		
+		public PixelColors(Pixel identityPixel) {
+			Pixel realPixel = getRealPixel(identityPixel);
+			BufferedImage boardImage = Board.getBoardImage();
+			Color pixelColors = new Color(boardImage.getRGB(realPixel.x, realPixel.y));
+			setPixelColors(pixelColors);
+		}
+		
+		private Pixel getRealPixel(Pixel identityPixel){
+			Pixel realPixel = new Pixel(identityPixel.x, identityPixel.y);
+			realPixel.x += squareOffset.x;
+			realPixel.y += squareOffset.y;
+			return realPixel;
+		}
+		
+		private void setPixelColors(Color pixelColors){
+			this.red = pixelColors.getRed();
+			this.green = pixelColors.getGreen();
+			this.blue = pixelColors.getBlue();
+		}
 	}
 }
